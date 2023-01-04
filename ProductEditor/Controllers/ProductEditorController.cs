def7a839981027d9f86a05b5920d68f45ca8642a
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-04 14:49:14 +0000

    added more empty methods

---
+++ ProductEditor/Controllers/ProductEditorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//namespace ProductSeller.DatabaseHandler.Product
+namespace ProductEditor.Controllers
+{
+    public class ProductEditorController : Controller
+    {
+
+        public void AlterProductPrice(int x, int ProductID)
+        {
+
+        }
+
+        public void ViewProductPrice(int ProductID)
+        {
+
+        }
+    }
+}
---
+++ StockService/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public int IncreaseStock()
+        {
+            return 0;
+        }
+        public int ReduceStock()
+        {
+            return 0;
+        }
+
+    }
+}
