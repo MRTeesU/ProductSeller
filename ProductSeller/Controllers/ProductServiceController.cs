540a8ed80d66973dc267f9bb1b6b938ca260cff2
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-04 14:59:04 +0000

    removed excess from product seller container

---
+++ ProductSeller/Controllers/ProductServiceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    public class ProductServiceController : Controller
+    {
+        public void BrowseProduct() { 
+
+        }
+        public void FilterProduct() { 
+
+        }
+        public void SearchProduct() { 
+
+        }
+        public void SubmitProduct() { 
+
+        }
+
+    }
+}
