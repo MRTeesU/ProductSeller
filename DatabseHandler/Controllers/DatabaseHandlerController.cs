3af5db79231a02453c9fd052162cdd3fee95d78f
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-05 14:52:16 +0000

    updated DatabaseHandlerController

--- DatabseHandler/Controllers/DatabaseHandlerController.cs
+++ DatabseHandler/Controllers/DatabaseHandlerController.cs
@@ -5,7 +5,20 @@ namespace DatabaseHandler.Controllers
 {
     public class DatabaseHandlerController : Controller
     {
-        
+        private HttpClient Client { get; }
+
+        record ProductDto(int ID,string name, string description, int price, bool InStock);
+
+        public DatabaseHandlerController(HttpClient client , IConfiguration configuration)
+        {
+            var baseUrl = configuration["WebServices:Comments:BaseURL"];
+            client.BaseAddress = new System.Uri(baseUrl);
+            client.Timeout = TimeSpan.FromSeconds(5);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            Client = client;
+        }
+
         public void ViewDatabase()
         {
             View();
